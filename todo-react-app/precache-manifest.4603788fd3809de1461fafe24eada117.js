89defe747a4965f4df35e0e3192caa9ff3d56fca
Ihor <ihor@Ihor-MacBook-Pro.local>
2019-08-08 23:44:28 +0300

    Todo React App

---
+++ todo-react-app/precache-manifest.4603788fd3809de1461fafe24eada117.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d82506f6b543b499b39ddd2384272e08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3d887a3c6eacfd1798b",
+    "url": "/static/css/main.22341a9c.chunk.css"
+  },
+  {
+    "revision": "06f44d7c6eeee966ea35",
+    "url": "/static/js/2.34ad89f7.chunk.js"
+  },
+  {
+    "revision": "f3d887a3c6eacfd1798b",
+    "url": "/static/js/main.f2e1c7d7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
